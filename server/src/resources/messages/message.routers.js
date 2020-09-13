7b420b97366959d4987c9a3b5b7c4ebb700b8de2
tarek tito <tarekt820@gmail.com>
2020-09-13 21:09:32 +0200

    feat() :: add routes for update,allmessage,reply apis

--- server/src/resources/messages/message.routers.js
+++ server/src/resources/messages/message.routers.js
@@ -1,11 +1,10 @@
 const express = require('express');
 const routers = express.Router();
-const { createMessage } = require('./message.controllers');
+const { createMessage , getAllMessages , updateMessage ,replyToMessage} = require('./message.controllers');
 
-// routers.get('/',getAllMessages);
-// routers.patch('/',updateMessage);
+routers.get('/',getAllMessages);
+routers.patch('/:id',updateMessage);
 routers.post('/',createMessage);
-// routers.get('/me',getMyMessages);
-// routers.post('/reply/:id',replyToMessage);
+routers.post('/reply/:id',replyToMessage);
 
 module.exports = routers;
