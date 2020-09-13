73db313673d90383fe592761d635643138875592
tarek tito <tarekt820@gmail.com>
2020-09-13 16:07:14 +0200

    feat() :: add auth routers

--- server/src/server.js
+++ server/src/server.js
@@ -1,29 +1,36 @@
+// imports 
+const express = require('express');
+const cookieParser = require('cookie-parser');
+const connect = require ('./utilities/db');
+require('dotenv').config();
+
+const app = express();
+
+// routers 
+const authRoutes = require('./utilities/auth/auth.routers');  
+
+
+// middlewares 
+app.use(express.urlencoded({extended:false}));
+app.use(express.json());
+app.use(cookieParser());
+
+
+// Routes
+app.use('/auth',authRoutes);
+
 async function startServer () {
-    
-  // imports 
-  const express = require('express');
-  const cookieParser = require('cookie-parser');
-  const connect = require ('./utilities/db');
-  require('dotenv').config();
-  
-  const app = express();
-  
-  // routers 
-//   const authRoutes = require('./utilities/auth/auth.routers');  
-  
-  
-  // middlewares 
-  app.use(express.urlencoded({extended:false}));
-  app.use(express.json());
-  app.use(cookieParser());
-  
-  
-  // Routes
-//   app.use('/auth',authRoutes);
-  await connect (process.env.DATABASE,process.env.PORT);
-  
+    try {
+        await connect (process.env.DATABASE,process.env.PORT);
+        app.listen(process.env.PORT,() => {
+            console.log(`REST API on http://localhost:${process.env.PORT}/api`)
+        })
+    } catch (e) {
+        console.error(e);
+    }
 }
-     
+
+
 
 module.exports = startServer;
     
