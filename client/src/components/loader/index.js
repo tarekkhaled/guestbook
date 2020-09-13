f7292bf60ddba9b61a84f69b356740a962285997
tarek tito <tarekt820@gmail.com>
2020-09-13 22:09:43 +0200

    Style() :: style loading component

---
+++ client/src/components/loader/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Loader() {
+    return (
+        <div className="Loader">
+            <div className="Loader__loading">
+            </div>
+        </div>
+    )
+}
+
+export default Loader
