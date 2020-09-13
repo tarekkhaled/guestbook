69424efb27a5f2f890a9c43e92cfcbeb1491106a
tarek tito <tarekt820@gmail.com>
2020-09-13 22:11:29 +0200

    Fix() :: fix typo

--- client/src/routes.js
+++ client/src/routes.js
@@ -2,18 +2,18 @@ import React,{Suspense,lazy} from 'react';
 import {Switch,Route} from 'react-router-dom';
 import Loader from './components/loader';
 const Login = lazy(() => import('./components/login'));
-const SignUP = lazy(() => import('./components/SignUP'));
-const Messages = lazy(() => import('./components/Messages'));
-const Profile = lazy(() => import('./components/Profile'));
+const SignUP = lazy(() => import('./components/signup'));
+const Messages = lazy(() => import('./components/messages'));
+const Profile = lazy(() => import('./components/profile'));
 
 
 const routes = () => (
     <Suspense fallback={<Loader/>}>
         <Switch>
-            <Route exact='/login' component={Login}/>
-            <Route exact='/signup' component={SignUP}/>
-            <Route exact='/messages' component={Messages}/>
-            <Route exact='/Profile' component={Profile}/>
+            <Route exact path='/login' component={Login}/>
+            <Route exact path='/signup' component={SignUP}/>
+            <Route exact path='/messages' component={Messages}/>
+            <Route exact path='/Profile' component={Profile}/>
         </Switch>
     </Suspense>
 )
