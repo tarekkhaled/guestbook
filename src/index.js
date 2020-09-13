c8e9deb1009b7e4d7ddb5c60ea71cb0fdafbbefa
tarek tito <tarekt820@gmail.com>
2020-09-13 14:41:30 +0200

    refactor() :: move App.js inside index.js as seperated Component

--- src/index.js
+++ src/index.js
@@ -1,7 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {BrowserRouter} from 'react-router-dom';
+import Routes from './routes';
 import './index.css';
-import App from './App';
+
+const App = () => (
+  <BrowserRouter>
+    <Routes/>
+  </BrowserRouter>
+)
+
 
 ReactDOM.render(
   <React.StrictMode>
