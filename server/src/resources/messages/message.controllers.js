c508f4847299167f1a4d2f806f01cd3be15951a0
tarek tito <tarekt820@gmail.com>
2020-09-13 21:09:01 +0200

    feat() :: add getAllMessages,updateMessage,replyToMessages

--- server/src/resources/messages/message.controllers.js
+++ server/src/resources/messages/message.controllers.js
@@ -11,8 +11,70 @@ const createMessage = async (req,res) => {
     }
 }
 
+const getAllMessages = async (req,res) => {
+  try {
+    const docs = await MessageModel
+      .find()
+      .lean()
+      .exec()
+
+    res.status(200).json({ data: docs })
+  } catch (e) {
+    console.error(e)
+    res.status(400).end()
+  }
+}
+
+const updateMessage = async (req,res) => {
+  try {
+    const updatedDoc = await MessageModel
+      .findOneAndUpdate(
+        {
+          createdBy: req.user._id,
+          _id: req.params.id
+        },
+        req.body,
+        { new: true }
+      )
+      .lean()
+      .exec()
+
+    if (!updatedDoc) {
+      return res.status(400).end()
+    }
+
+    res.status(200).json({ data: updatedDoc })
+  } catch (e) {
+    console.error(e)
+    res.status(400).end()
+  }
+}
+
+const replyToMessage = async (req,res) => {
+  try {
+    const message = await MessageModel.findOne({_id: req.params.id})
+        .lean()
+        .exec()
+
+      if (!message) {
+        return res.status(400).end()
+      }
+      message.replies.push({...req.body,replyBy:req.user._id});
+      res.status(200).json({
+        success:true,
+        message
+      })
+  } catch (e) {
+    console.error(e)
+    res.status(400).end()
+  }
+}
+
 
 
 module.exports = {
-    createMessage
+    createMessage,
+    getAllMessages,
+    updateMessage,
+    replyToMessage
 }
