c38ff712a3134ef50646593ab30c391cad8432a9
tarek tito <tarekt820@gmail.com>
2020-09-13 16:43:37 +0200

    feat() :: add sign in /login to routes

--- server/src/utilities/auth/auth.routers.js
+++ server/src/utilities/auth/auth.routers.js
@@ -4,9 +4,9 @@ const {signUserIn,signUserOut,signUserUp} = require('./auth.controllers');
 
 
 
-// router.use('/login',signUserIn);
+router.post('/login',signUserIn);
 router.post('/signup',signUserUp)
-router.use('/logout',signUserOut);
+router.get('/logout',signUserOut);
 
 
 module.exports =  router;
