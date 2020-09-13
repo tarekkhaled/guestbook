bcb2e16fb68b745da31417aafa594abe198aedc0
tarek tito <tarekt820@gmail.com>
2020-09-13 15:07:02 +0200

    feat() :: add express,app init the server

---
+++ server/src/server.js
@@ -0,0 +1,29 @@
+async function startServer () {
+    
+  // imports 
+  const express = require('express');
+  const cookieParser = require('cookie-parser');
+  const connect = require ('../utilites/db');
+  require('dotenv').config();
+  
+  const app = express();
+  
+  // routers 
+  const authRoutes = require('../utilites/auth/auth.routers');  
+  
+  
+  // middlewares 
+  app.use(express.urlencoded({extended:false}));
+  app.use(express.json());
+  app.use(cookieParser());
+  
+  
+  // Routes
+  app.use('/auth',authRoutes);
+  await connect (process.env.DATABASE,process.env.PORT);
+  
+}
+     
+
+module.exports = startServer;
+    
