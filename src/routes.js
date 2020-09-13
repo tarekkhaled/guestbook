97826992401ad754fde63fd797864a7355eeb7c0
tarek tito <tarekt820@gmail.com>
2020-09-13 14:37:44 +0200

    feat() :: add lazy loading for the components (suspense)

--- src/routes.js
+++ src/routes.js
@@ -1,18 +1,21 @@
-import React from 'react';
+import React,{Suspense,lazy} from 'react';
 import {Switch,Route} from 'react-router-dom';
-import Login from './components/login';
-import SignUP from './components/signup';
-import Messages from './components/messages';
-import Profile from './components/profile';
+import Loader from './components/loader';
+const Login = lazy(() => import('./components/login'));
+const SignUP = lazy(() => import('./components/SignUP'));
+const Messages = lazy(() => import('./components/Messages'));
+const Profile = lazy(() => import('./components/Profile'));
 
 
 const routes = () => (
+    <Suspense fallback={<Loader/>}>
         <Switch>
             <Route exact='/login' component={Login}/>
             <Route exact='/signup' component={SignUP}/>
             <Route exact='/messages' component={Messages}/>
             <Route exact='/Profile' component={Profile}/>
         </Switch>
+    </Suspense>
 )
 
 export default routes;
