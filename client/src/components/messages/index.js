7ef9bcda69e9ec1299aa3c74b8a8a36cf50233c8
tarek tito <tarekt820@gmail.com>
2020-09-13 22:11:55 +0200

    init() :: add the three other components

---
+++ client/src/components/messages/index.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class Messages extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
+
+export default Messages;
