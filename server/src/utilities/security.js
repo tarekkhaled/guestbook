2475640cba23be47d1cb159df158c60248d06e63
tarek tito <tarekt820@gmail.com>
2020-09-13 17:14:02 +0200

    feat() :: add permission method to protect Apis

--- server/src/utilities/security.js
+++ server/src/utilities/security.js
@@ -29,6 +29,36 @@ function hashThePassword (next) {
 }
 
 
+export const permission = async (req, res, next) => {
+    const bearer = req.headers.authorization
+  
+    if (!bearer || !bearer.startsWith('Bearer ')) {
+      return res.status(401).end()
+    }
+  
+    const token = bearer.split('Bearer ')[1].trim()
+    let payload
+    try {
+      payload = await verifyToken(token)
+    } catch (e) {
+      return res.status(401).end()
+    }
+  
+    const user = await User.findById(payload.id)
+      .select('-password')
+      .lean()
+      .exec()
+  
+    if (!user) {
+      return res.status(401).end()
+    }
+  
+    req.user = user
+    next()
+}
+  
+
+
 module.exports = {
     createToken,
     verifyToken,
