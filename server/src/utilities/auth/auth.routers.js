e4fd8078b57759751beae995237c10afe60a0d8e
tarek tito <tarekt820@gmail.com>
2020-09-13 16:31:27 +0200

    feat() :: add route for signout method

--- server/src/utilities/auth/auth.routers.js
+++ server/src/utilities/auth/auth.routers.js
@@ -6,7 +6,7 @@ const {signUserIn,signUserOut,signUserUp} = require('./auth.controllers');
 
 // router.use('/login',signUserIn);
 router.post('/signup',signUserUp)
-// router.use('/logout',signUserOut);
+router.use('/logout',signUserOut);
 
 
 module.exports =  router;
