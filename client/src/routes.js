99b76edc968e2f5c9b4c76fb507d09a526f0f9ee
tarek tito <tarekt820@gmail.com>
2020-09-14 04:02:06 +0200

    refactor() :: replace messages with home

--- client/src/routes.js
+++ client/src/routes.js
@@ -3,16 +3,16 @@ import {Switch,Route} from 'react-router-dom';
 import Loader from './components/loader';
 const Login = lazy(() => import('./components/login'));
 const SignUP = lazy(() => import('./components/signup'));
-const Messages = lazy(() => import('./components/messages'));
+const Home = lazy(() => import('./components/home'));
 const Profile = lazy(() => import('./components/profile'));
 
 
 const routes = () => (
     <Suspense fallback={<Loader/>}>
         <Switch>
+            <Route exact path='/home' component={Home}/>
             <Route exact path='/login' component={Login}/>
             <Route exact path='/signup' component={SignUP}/>
-            <Route exact path='/messages' component={Messages}/>
             <Route exact path='/Profile' component={Profile}/>
         </Switch>
     </Suspense>
