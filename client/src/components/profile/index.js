1506da7e525264c63077c8827b333a6f7ba19d85
tarek tito <tarekt820@gmail.com>
2020-09-14 02:20:48 +0200

    feat() :: done with profile , add navbar  , logout fn

--- client/src/components/profile/index.js
+++ client/src/components/profile/index.js
@@ -1,10 +1,62 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import './profile.css';
+import '../signup/signup.css';
+import {getW_authCookie, config} from '../../utilities';
+import axios from 'axios';
+import Navbar from '../navbar';
 
 class Profile extends Component {
+    componentDidMount() {
+        if(!getW_authCookie()) {
+            window.location.assign('/login/')
+        }
+    }
+    
+    state = {
+        formSuccess: false,
+        formData: {
+          message: ''
+        },
+      };
+
+    updateMessage = (e) => {
+        const {formData} = this.state;
+        const newFormData = {...formData};
+        newFormData['message'] = e.target.value;
+        this.setState({
+            formData:newFormData
+        })
+    }
+
+    handleFormSubmit = async (e) => {
+        e.preventDefault();
+        const { formData,formData:{message} } = this.state;
+        if(!message) return alert('write a message please !');
+        console.log({formData})
+        const response = await axios.post('/api/messages/',formData,config(getW_authCookie()));
+        console.log({response})
+        if(response.status === 201) {
+          localStorage.setItem('message', JSON.stringify(response.data.data));
+          window.location.reload();
+        }
+         
+        
+    }
     render() {
         return (
-            <div>
-                
+            <div className="Profile">
+                <Navbar/>
+                <form
+                    className="signup__form"
+                    autoComplete="off"
+                    onSubmit={this.handleFormSubmit}
+                >
+            <h2>Create New Message </h2>
+            <textarea className="Profile__textarea" name="message" placeholder="write your message here..." onChange={this.updateMessage}></textarea>
+          <button className="signup__form_submit" type="submit">
+            Submit
+          </button>
+        </form>
             </div>
         )
     }
